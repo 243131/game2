956c1a9f68e5749c8f3cb26a60085f37c9f35f62
Sul243131 <sulaimaan21109@hotmail.com>
2025-01-07 12:28:41 +0000

    a

--- game2/Form1.cs
+++ game2/Form1.cs
@@ -1,36 +1,36 @@
-﻿using game2.Properties;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿    using game2.Properties;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
 
-namespace game2
-{
-    public partial class Form1 : Form
+    namespace game2
     {
-        char[] characters = new char[14];
-        string[] newword = new string[3];
-        string[] alphabet = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-        string[] sigma = new string[] { "b", "a", "n", "e", "s", "b", "a", "r", "e", "s", "b", "e", "a", "n", "s" };
-        int labelcounter = 1;
-        int xx = 400;
-        int yy = 100;
-        Random rnd = new Random();
-        List<string> wordList = new List<string>();
-        List<char> allCharacters = new List<char>();
-        public Form1()
+        public partial class Form1 : Form
         {
-            InitializeComponent();
+            char[] characters = new char[14];
+            string[] newword = new string[3];
+            string[] alphabet = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            int labelcounter = 1;
+            int xx = 400;
+            int yy = 100;
+            Random rnd = new Random();
+            List<string> wordList = new List<string>();
+            List<char> allCharacters = new List<char>();
+            
+            public Form1()
+            {
+                InitializeComponent();
 
 
-            StreamReader file = new StreamReader("C:\\Users\\sulai\\source\\repos\\game2\\game2\\bin\\Debug\\ukenglish.txt");
+                StreamReader file = new StreamReader("words.txt");
             {
                 string word;
                 while ((word = file.ReadLine()) != null)
@@ -42,101 +42,164 @@ namespace game2
                 }
                 file.Close();
             }
-            
-            for (int i = 0; i < 3; i++)
+            int wordnumchoose = rnd.Next(0, wordList.Count);
+        }
+
+           
+            private void AddLabel(String msg, int x, int y, String labelName)
             {
-                int wordnumchoose = rnd.Next(0,wordList.Count);
-                newword[i] = wordList[wordnumchoose];
-                allCharacters.AddRange(newword[i].ToCharArray());
+                Label lbl = new Label();
+                lbl.Location = new Point(x, y);
+                lbl.Size = new Size(50, 50);
+                lbl.BackColor = Color.Transparent;
+                lbl.BorderStyle = BorderStyle.FixedSingle;
+                lbl.Text = msg;
+                lbl.TextAlign = ContentAlignment.MiddleCenter;
+                lbl.Font = new Font("Colonna MT", 12);
+                lbl.Name = labelName;
+                this.Controls.Add(lbl);
             }
-          characters = allCharacters.ToArray();
-        
-            while (yy < 400)
+
+            private void DrawEllipseInt(PaintEventArgs e)
             {
-                for (int i = 0; i < characters.Length; i++)
+
+                Pen blackPen = new Pen(Color.Black, 3);
+
+                int x = 475;
+                int y = 350;
+                int width = 200;
+                int height = 200;
+
+            
+                e.Graphics.DrawEllipse(blackPen, x, y, width, height);
+            }
+            protected override void OnPaint(PaintEventArgs e)
+            {
+                if (wordList.Count == 0)
                 {
-                    AddLabel(characters[i].ToString(), xx, yy, "label_" + labelcounter);
-                    labelcounter++;
-                    xx = xx + 50;
+                MessageBox.Show("No 5-letter words found in ukenglish.txt.");
+                return;
+                }
+            base.OnPaint(e);
+                int xxx = 500;
+                int yyy = 375;
+                int j = 1;
+                bool vowelAdded = false;
+                int[] vowels = { 0,4,8,14,20 };
+                int totalLetters = 5;
+                int[] selectedletters = new int[totalLetters];
 
-
-                    if (xx > 600)
+            for (int i = 0; i < totalLetters; i++)
+            {
+                int letternum;
+                if (!vowelAdded && (i == totalLetters - 1 || rnd.Next(0, 2) == 0))
+                { 
+                        letternum = vowels[rnd.Next(0,vowels.Length)];
+                    vowelAdded = true;
+                }
+                else
+                {
+                    letternum = rnd.Next(0, alphabet.Length);
+                    if (vowelchecker(letternum))
                     {
-                        yy = yy + 100;
-                        xx = 400;
+                        vowelAdded = true;
                     }
                 }
-                break;
-            } 
-           
-        }
-
-           
-        private void AddLabel(String msg, int x, int y, String labelName)
-        {
-            Label lbl = new Label();
-            lbl.Location = new Point(x, y);
-            lbl.Size = new Size(50, 50);
-            lbl.BackColor = Color.Transparent;
-            lbl.BorderStyle = BorderStyle.FixedSingle;
-            lbl.Text = msg;
-            lbl.TextAlign = ContentAlignment.MiddleCenter;
-            lbl.Font = new Font("Colonna MT", 12);
-            lbl.Name = labelName;
-            this.Controls.Add(lbl);
-        }
-
-        private void DrawEllipseInt(PaintEventArgs e)
-        {
-
-            Pen blackPen = new Pen(Color.Black, 3);
-
-            int x = 475;
-            int y = 350;
-            int width = 200;
-            int height = 200;
-
-            
-            e.Graphics.DrawEllipse(blackPen, x, y, width, height);
-        }
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            
-            base.OnPaint(e);
-            int xxx = 500;
-                int yyy = 375;
-            int j = 1;
-            for (int i = 1; i <= 5; i++)
-            {
-                
-                int letternum = rnd.Next(0, 25);
+                selectedletters[i] = letternum;
                 AddLabel(alphabet[letternum], xxx, yyy, "label_" + labelcounter);
-                labelcounter++;
-                if (i <= 2)
-                {
-                    xxx = xxx + 100;
-                }
-                if (j >= 2)
-                {
-                    yyy = yyy + 75;
-                    xxx = 325;
-                    j = 0;
-                    xxx = xxx + 150;
-                }
-                if (i == 3)
-                {
-                    xxx = xxx + 150;
-                }
-                if (i >= 4)
-                {
-                    xxx = 400;
-                    yyy = 500;
-                    xxx = xxx + 150;
-                }
+                    labelcounter++;
+                   
+                    if (i <= 1)
+                    {
+                        xxx = xxx + 100;
+                    }
+                    if (j >= 2)
+                    {
+                        yyy = yyy + 75;
+                        xxx = 325;
+                        j = 0;
+                        xxx = xxx + 150;
+                    }
+                    if (i == 2)
+                    {
+                        xxx = xxx + 150;
+                    }
+                    if (i >= 3)
+                    {
+                        xxx = 400;
+                        yyy = 500;
+                        xxx = xxx + 150;
+                    }
               
-                j++;
+                    j++;
+
             }
+
             DrawEllipseInt(e);
+            HashSet<char> selectedLettersSet = new HashSet<char>();
+            foreach (int letterIndex in selectedletters)
+            {
+                selectedLettersSet.Add(alphabet[letterIndex][0]); 
+            }
+
+            List<string> validWords = wordList.Where(word => word.All(letter => selectedLettersSet.Contains(letter))).ToList();
+
+            if (validWords.Count == 0)
+            {
+
+                return;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                int wordnumchoose = rnd.Next(0, validWords.Count);
+                newword[i] = validWords[wordnumchoose];
+                allCharacters.AddRange(newword[i].ToCharArray());
+            }
+              characters = allCharacters.ToArray();
+        
+                while (yy < 400)
+                {
+                    for (int i = 0; i < characters.Length; i++)
+                    {
+                        AddLabel(characters[i].ToString(), xx, yy, "label_" + labelcounter);
+                        labelcounter++;
+                        xx = xx + 50;
+
+
+                        if (xx > 600)
+                        {
+                            yy = yy + 100;
+                            xx = 400;
+                        }
+                    }
+                    break;
+                } 
         }
+        public static bool vowelchecker(int value)
+        {
+            switch (value)
+            {
+                case 0:
+                    return true;
+
+                case 4:
+                    return true;
+
+                case 8:
+                    return true;
+
+                case 14:
+                    return true;
+
+                case 20:
+                    return true;
+            }
+
+            return false;
+        }
+
+
     }
 }
+    
